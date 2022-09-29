fd15a7beac04453819ac446d12b5a627b97cf5bc
Raneet Roy <raneetroy20@gmail.com>
2022-09-29 19:52:33 +0530

    added some spaces between codes

--- src/index.js
+++ src/index.js
@@ -12,24 +12,41 @@
 // Step 11: Modifying table attribute properties 
 
 var center = document.createElement('center');
+
 var ChessTable = document.createElement('table');
+
 for (var i = 0; i < 8; i++) {
+
     var tr = document.createElement('tr');
+
     for (var j = 0; j < 8; j++) {
+
         var td = document.createElement('td');
+
         if ((i + j) % 2 == 0) {
+
             td.setAttribute('class', 'box whitebox');
+
             tr.appendChild(td);
+
         }
+
         else {
+
             td.setAttribute('class', 'box blackbox');
 
             tr.appendChild(td);
         }
     }
+
     ChessTable.appendChild(tr);
+
 }
+
 center.appendChild(ChessTable);
+
 ChessTable.setAttribute('cellspacing', '0');
+
 ChessTable.setAttribute('width', '270px');
+
 document.body.appendChild(center);
